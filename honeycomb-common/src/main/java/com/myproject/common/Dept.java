721f7a073357e1ed1964a51f6de89919086cd347
13375 <1337515006@qq.com>
2020-06-02 18:42:04 +0800

    6.2创建springcloud项目

---
+++ honeycomb-common/src/main/java/com/myproject/common/Dept.java
@@ -0,0 +1,17 @@
+package com.myproject.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author shuyi
+ * @date 2020/6/2
+ */
+@Data
+public class Dept implements Serializable {
+    private static final long serialVersionUID = -7731865410220747519L;
+    private Integer deptNo;
+    private String deptName;
+    private String dbSource;
+}
